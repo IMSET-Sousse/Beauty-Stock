d18b8b40424725e8574fa568b87d84b43aed86ae
HELA-GARNAOUI <168767678+HELA-GARNAOUI@users.noreply.github.com>
2025-04-07 21:48:57 +0100

    Remove unused Produit app and related files

--- Produit/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'blog'
-urlpatterns = [
-    path('', views.liste_articles, name='liste_articles'),
-]
--- Produit/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from .models import Article
-
-def liste_articles(request):
-    articles = Article.objects.all().order_by('-date_creation')
-    return render(request, 'blog/liste_articles.html', {'articles': articles})

6630676c782a513cea556f11b409b2477f34acc7
HELA-GARNAOUI <168767678+HELA-GARNAOUI@users.noreply.github.com>
2025-04-21 21:01:18 +0100

    Add 'product' app to INSTALLED_APPS and update URL patterns for products and dashboard

--- app/settings.py
+++ app/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'products',
     'categories' , 
     'stock' ,
+    'product',
 ]
 
 MIDDLEWARE = [
--- app/urls.py
+++ app/urls.py
@@ -3,12 +3,7 @@ from django.urls import path, include
 from django.views.generic import RedirectView
 urlpatterns = [
     path('admin/', admin.site.urls),
-<<<<<<< HEAD
-    path('product/', include('product.urls')),
-    path('', views.home, name='home'),
-    path('dashboard/', include('dashboard.urls')),
-=======
-    path('stock/', include('stock.urls')),
+    path('products/', include('products.urls')),
     path('', RedirectView.as_view(url='/stock/home/')),  
->>>>>>> a79e0c3354aaa37c9598c1f698af23f4c998c8eb
+    path('dashboard/', include('dashboard.urls')),
 ]
